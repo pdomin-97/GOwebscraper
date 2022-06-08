54e1eb444cceb85b12d68135b1f2685424d4ace9
Przemek <p.domin97@gmail.com>
2022-06-08 14:47:10 +0200

    Managed to actually get the data, still struggling with saving it properly as CSV file - whole data is being added as a header

--- main.go
+++ main.go
@@ -1,63 +1,103 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
-func main() {
-	crawl()
+type StockPrices struct {
+	Name  string
+	Price string
 }
 
-func crawl() {
+func main() {
 
-	type data struct {
-		City               string
-		Type_of_employment string
-		Experience         string
-		Company            string
+	// saving the data as a .csv file
+	file, err := os.Create("CryptoData.csv")
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// write CSV header
+
+	headers := []string{"Name", "Price"}
+	writer.Write(headers)
+
+	// actually scraping the data
 	c := colly.NewCollector()
-	infoCollector := c.Clone()
+
+	c.OnHTML(".lcw-table-container.main-table", func(e *colly.HTMLElement) {
+
+		data := StockPrices{}
+		data.Name = e.ChildText(".filter-item-name.mb0.text-left")
+		data.Price = e.ChildText("td.filter-item.table-item.main-price")
+
+		row := []string{data.Name, data.Price}
+		writer.Write(row)
+
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println(r.StatusCode)
+	})
 
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting: ", r.URL.String())
+		fmt.Println("Visiting", r.URL)
 	})
 
-	infoCollector.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting Profile URL: ", r.URL.String())
-	})
-
-	c.Visit("https://justjoin.it/all/go")
-
-	c.OnHTML(".css-1smbjja", func(e *colly.HTMLElement) {
-		nextPage := e.Request.AbsoluteURL(e.Attr("href"))
-		c.Visit(nextPage)
-	})
-
-	c.OnHTML(".mode-detail", func(e *colly.HTMLElement) {
-		profileUrl := e.ChildAttr(".css-ic7v2w div", "href")
-		profileUrl = e.Request.AbsoluteURL(profileUrl)
-		infoCollector.Visit(profileUrl)
-	})
-
-	infoCollector.OnHTML(".css-vuh3mm", func(e *colly.HTMLElement) {
-		tmpProfile := data{}
-		tmpProfile.City = e.ChildText("span.css-9wmrp4")
-		tmpProfile.Type_of_employment = e.ChildText("span.css-ioglek")
-		tmpProfile.Experience = e.ChildText(".css-1ji7bvd")
-		tmpProfile.Company = e.ChildText(".css-1npsplc")
-
-		js, err := json.MarshalIndent(tmpProfile, "", "    ")
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(string(js))
-
-	})
+	c.Visit("https://www.livecoinwatch.com/")
 
 }
+
+// package main
+
+// import (
+// 	"encoding/csv"
+// 	"log"
+// 	"os"
+
+// 	"github.com/gocolly/colly"
+// )
+
+// func main() {
+// 	fName := "cryptocoinmarketcap.csv"
+// 	file, err := os.Create(fName)
+// 	if err != nil {
+// 		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+// 		return
+// 	}
+// 	defer file.Close()
+// 	writer := csv.NewWriter(file)
+// 	defer writer.Flush()
+
+// 	// Write CSV header
+// 	writer.Write([]string{"Name", "Symbol", "Price (USD)", "Volume (USD)", "Market capacity (USD)", "Change (1h)", "Change (24h)", "Change (7d)"})
+
+// 	// Instantiate default collector
+// 	c := colly.NewCollector()
+
+// 	c.OnHTML("#currencies-all tbody tr", func(e *colly.HTMLElement) {
+// 		writer.Write([]string{
+// 			e.ChildText(".currency-name-container"),
+// 			e.ChildText(".col-symbol"),
+// 			e.ChildAttr("a.price", "data-usd"),
+// 			e.ChildAttr("a.volume", "data-usd"),
+// 			e.ChildAttr(".market-cap", "data-usd"),
+// 			e.ChildText(".percent-1h"),
+// 			e.ChildText(".percent-24h"),
+// 			e.ChildText(".percent-7d"),
+// 		})
+// 	})
+
+// 	c.Visit("https://coinmarketcap.com/all/views/all/")
+
+// 	log.Printf("Scraping finished, check file %q for results\n", fName)
+// }
